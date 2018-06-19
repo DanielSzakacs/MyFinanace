4ae4360b95b26cb5abe3eafc75191b0c2eef670c
danielszakacs <daniel.sokach@gmail.com>
2018-06-19 22:54:39 +0300

    exchange is ready

--- static/events/main_page_event.js
+++ static/events/main_page_event.js
@@ -10,9 +10,21 @@ for (let btn of headerButtons) {
 }
 
 
+var regLogButton = document.getElementsByClassName('user-reg-log-button');
+for (let button of regLogButton) {
+    button.addEventListener("mouseover", function () {
+        this.style.backgroundColor = "#ef9a9a"
+    })
+    button.addEventListener("mouseout", function () {
+        this.style.backgroundColor = "red"
+    })
+}
+
+
 // For the exchange data
 var exchangeButton = document.getElementById('exchange-button');
 exchangeButton.addEventListener('click', exchange());
+
 
 function exchange() {
     fetch('http://apilayer.net/api/live?access_key=4674244d882e754ce2c95f1f0fc907cc&currencies=EUR,GBP,RUB,HUF&source=USD&format=1')
@@ -27,3 +39,18 @@ function exchange() {
 }
 
 
+// TO get the user current place
+var accountButton = document.getElementById('account-button');
+accountButton.addEventListener('click', getAddress());
+
+function getAddress() {
+    fetch('http://api.ipstack.com/80.98.112.241?access_key=857bd1f1c26ae495c8078145822f24a4&format=1')
+    .then((response) => response.json())
+    .then((data) => {
+            document.getElementById('current-place').innerHTML = data.country_name
+            document.getElementById('city-name').innerHTML = data.region_name
+        })
+}
+
+// get your exchange data from this link
+// https://v3.exchangerate-api.com/bulk/d9e74d595777c01c2bff96e7/HUF
